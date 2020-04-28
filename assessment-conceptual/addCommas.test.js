31d80cfc9a55f35227cd7c86ce5b2439a048b637
Sivadej K <bombyk@gmail.com>
2020-04-28 08:54:06 -0800

    addCommas conceptual assignment/test

---
+++ assessment-conceptual/addCommas.test.js
@@ -0,0 +1,43 @@
+const addCommas = require('./addCommas');
+
+test('returns string', ()=> {
+  expect(addCommas(Math.random(1)*1000000)).toEqual(expect.any(String))
+  expect(addCommas('this is invalid')).toEqual(expect.any(String))
+})
+
+test('small numbers to return no commas', ()=> {
+  expect(addCommas(10)).toBe('10');
+  expect(addCommas(0)).toBe('0');
+  expect(addCommas(-999.99)).toBe('-999.99');
+});
+
+test('large positive integers', ()=> {
+  expect(addCommas(123456789)).toBe('123,456,789');
+  expect(addCommas(99999)).toBe('99,999');
+  expect(addCommas(1234)).toBe('1,234');
+});
+
+test('large postive floats', ()=> {
+  expect(addCommas(123456789.98)).toBe('123,456,789.98');
+  expect(addCommas(99999.99)).toBe('99,999.99');
+  expect(addCommas(1234.5)).toBe('1,234.5');
+});
+
+test('large negative floats', ()=> {
+  expect(addCommas(-123456789.98)).toBe('-123,456,789.98');
+  expect(addCommas(-99999.99)).toBe('-99,999.99');
+  expect(addCommas(-1234.5)).toBe('-1,234.5');
+});
+
+test('large negative integers', ()=> {
+  expect(addCommas(-123456789)).toBe('-123,456,789');
+  expect(addCommas(-99999)).toBe('-99,999');
+  expect(addCommas(-1234)).toBe('-1,234');
+});
+
+test('return error message on invalid arguments', ()=> {
+  expect(addCommas('this is invalid')).toMatch('invalid argument.');
+  expect(addCommas(['somearray'])).toMatch('invalid argument.');
+  expect(addCommas({some:'object'})).toMatch('invalid argument.');
+  expect(addCommas('123,456')).toMatch('invalid argument.');
+});
