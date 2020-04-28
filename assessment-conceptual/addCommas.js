3d7075951f240b2e092bbe76f60febd838b3da7c
Sivadej K <bombyk@gmail.com>
2020-04-28 10:01:56 -0800

    update test description

--- assessment-conceptual/addCommas.js
+++ assessment-conceptual/addCommas.js
@@ -4,22 +4,22 @@ const addCommas = (num) => {
   if (typeof(num) !== 'number') return 'invalid argument. number required'
   if (Math.abs(num) < 1000) return num.toString();
 
-  // 1. convert number to array
+  // convert number to array
   let numArr = Array.from(Math.abs(num).toString());
 
-  // 2. insert comma into 3rd-from-last element of end or decimal point
+  // insert comma into 3rd-from-last element of end or decimal point
   let firstIndex = numArr.indexOf('.') === -1 ? numArr.length : numArr.indexOf('.') ;
   numArr.splice(firstIndex-3, 0, ',');
 
-  // 3. identify index of first comma, insert comma into 3rd-from-last of this index.
-  //    repeat until indexOf no longer needed (when next index is 3 or less)
+  // identify index of first comma, insert comma into 3rd-from-last of this index.
+  // repeat until indexOf no longer needed (when next index is 3 or less)
   let nextIndexforComma = numArr.indexOf(',');
   while (nextIndexforComma > 3) {
     numArr.splice(nextIndexforComma-3,0,',');
     nextIndexforComma = numArr.indexOf(',');
   }
   
-  // 5. concat array to string and return
+  // concat array to string and return
   if (num < 0) return ('-'+numArr.join(''))
   else return numArr.join('');
 }
